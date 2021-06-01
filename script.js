4d05453b37fb3b1f127f41d2531a2ad474f5b17d
tncourtz <tncourtz@gmail.com>
2021-06-01 14:27:05 +0200

    Icon-TextFormat
    added bat signal icon
    and changed text colour to yellow

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ function initMap() {
 		mapId: '2cb7f76b2e2ea6ef',
 	});
 
+	//marker images'
+	const signal = "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+	const gotham = "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+	const cave = "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+   
+
     //The Marker Postions
 	// Name
 	// Latitude, Longitude
@@ -18,16 +24,19 @@ function initMap() {
 			"Gotham",
 			-33.90363,
             18.420529,
+			gotham,
 		],
 		[
 			'Bat Signal',
 			-33.9668233,
 			18.4080533,
+			signal,
 		],
 		[
 			'Bat Cave',
 			-33.957652,
 			18.4611991,
+			cave,
 		],
 	];
 
@@ -40,7 +49,7 @@ function initMap() {
 			title: currMarker[0],
 			icon: {
 				url: currMarker[3],
-				scaledSize: new google.maps.Size(currMarker[4], currMarker[5]),
+				// scaledSize: new google.maps.Size(currMarker[4], currMarker[5]),
 			},
 			animation: google.maps.Animation.DROP,
 		});
